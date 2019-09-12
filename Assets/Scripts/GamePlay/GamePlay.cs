9721df9fbe5fa94da2fe65bc8659914e62bf14eb
Kamran Elchuzade <elchuzade@gmail.com>
2019-09-12 20:08:36 +0300

    added stars disappearing functionality

--- Assets/Scripts/GamePlay/GamePlay.cs
+++ Assets/Scripts/GamePlay/GamePlay.cs
@@ -10,14 +10,14 @@ public class GamePlay : MonoBehaviour
     Player player;
     private int levelIndex;
     private int levelTaps;
-    public int starsAmount = 3;
+    public int starsAmount;
     private int totalCoins;
     [SerializeField] Text tapsText;
     [SerializeField] Text coinsText;
     [SerializeField] GameObject star1;
     [SerializeField] GameObject star2;
     [SerializeField] GameObject star3;
-
+    
     private void Awake()
     {
         player = FindObjectOfType<Player>();
@@ -31,13 +31,41 @@ public class GamePlay : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        switch (starsAmount)
+        {
+            case 3:
+                star1.GetComponent<Image>().enabled = true;
+                star2.GetComponent<Image>().enabled = true;
+                star3.GetComponent<Image>().enabled = true;
+                break;
+            case 2:
+                star1.GetComponent<Image>().enabled = true;
+                star2.GetComponent<Image>().enabled = true;
+                star3.GetComponent<Image>().enabled = false;
+                break;
+            case 1:
+                star1.GetComponent<Image>().enabled = true;
+                star2.GetComponent<Image>().enabled = false;
+                star3.GetComponent<Image>().enabled = false;
+                break;
+            default:
+                star1.GetComponent<Image>().enabled = false;
+                star2.GetComponent<Image>().enabled = false;
+                star3.GetComponent<Image>().enabled = false;
+                break;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void TouchedPaddle()
+    {
+        starsAmount = 2;
+        levelTaps = levelsData.allLevelsTapsAmounts[levelIndex];
     }
 
     public void ReturnToMainMenu()
